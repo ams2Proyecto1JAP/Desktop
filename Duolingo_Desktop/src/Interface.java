5e36863a6d4a93195486eba97ca44cae67c5e9c4
susom <susom@Susomatuso-Portatil>
2020-11-26 18:38:10 +0100

    13 14 DONE

---
+++ Duolingo_Desktop/src/Interface.java
@@ -0,0 +1,58 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import java.awt.Toolkit;
+
+public class Interface extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Interface frame = new Interface();
+					frame.setResizable(false);
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Interface() {
+		//Set iccon image of app
+		setIconImage(Toolkit.getDefaultToolkit().getImage("resources/duolingo.png"));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		setExtendedState(JFrame.MAXIMIZED_BOTH);
+		//JMenuBar setter
+		JMenuBar jMenuBar = new JMenuBar();
+		setJMenuBar(jMenuBar);
+		
+		//JMenuBar Add Items
+		JMenu jMenu = new JMenu("Menu");
+		jMenuBar.add(jMenu);
+		
+		//JMenu Add Items
+		jMenu.add("Administrar Cursos");
+		jMenu.add("Altres");
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
